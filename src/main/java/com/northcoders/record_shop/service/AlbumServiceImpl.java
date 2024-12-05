e9619435e6d3495b11e221d448e1671c77121929
benniwilson <benniwilson7@gmail.com>
2024-12-05 11:39:15 +0000

    feat: Added a getAllBooks in the AlbumService interface and implemented the method in the AlbumServiceImpl class using methods from the AlbumRepository interface

--- src/main/java/com/northcoders/record_shop/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/record_shop/service/AlbumServiceImpl.java
@@ -1,4 +1,21 @@
 package com.northcoders.record_shop.service;
 
+import com.northcoders.record_shop.model.Album;
+import com.northcoders.record_shop.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlbumServiceImpl implements AlbumService {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
+    }
 }
