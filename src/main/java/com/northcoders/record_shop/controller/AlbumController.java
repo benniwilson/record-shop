7fd7d2e18fa49fcb854bf0bfd71757ce9049e065
benniwilson <benniwilson7@gmail.com>
2024-12-05 10:29:34 +0000

    feat: Created a controller package which contains an AlbumController class with an annotated base request path

---
+++ src/main/java/com/northcoders/record_shop/controller/AlbumController.java
@@ -0,0 +1,10 @@
+package com.northcoders.record_shop.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recordshop")
+public class AlbumController{
+}
