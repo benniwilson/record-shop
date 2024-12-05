b2ed51282e883f23b4acbd18eae970b4eff44c82
benniwilson <benniwilson7@gmail.com>
2024-12-05 16:00:23 +0000

    feat: Added a new method getAlbumById in the RecordShopService interface and implementation for this method in the RecordShopServiceImpl class, also added the corresponding tests in the test class

--- src/main/java/com/northcoders/record_shop/service/RecordShopService.java
+++ src/main/java/com/northcoders/record_shop/service/RecordShopService.java
@@ -3,7 +3,9 @@ package com.northcoders.record_shop.service;
 import com.northcoders.record_shop.model.Album;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecordShopService {
     public List<Album> getAllAlbums();
+    public Optional<Album> getAlbumById(Long id);
 }
--- src/test/java/com/northcoders/record_shop/service/RecordShopServiceTest.java
+++ src/test/java/com/northcoders/record_shop/service/RecordShopServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -71,5 +72,34 @@ class RecordShopServiceTest {
         assertThat(result).isEqualTo(albumList);
         assertThat(result).hasSize(2);
     }
+
+    @Test
+    @DisplayName("Method returns optional empty if id doesn't correspond to an album")
+    public void test_getAlbumByIdWrongId(){
+        long id = 1;
+        Optional<Album> album = Optional.empty();
+        when(albumRepository.findById(id)).thenReturn(album);
+        Optional<Album> result = albumService.getAlbumById(id);
+        assertEquals(album, result);
+    }
+
+    @Test
+    @DisplayName("Method returns album that the id corresponds to")
+    public void test_getAlbumById(){
+        long id = 27;
+        Optional<Album> album = Optional.of(Album.builder()
+                .id(27L)
+                .name("ASTROWORLD")
+                .stock(8)
+                .genre(Genre.Rap)
+                .price(10.99)
+                .artist("Travis Scott")
+                .dateReleased(LocalDate.of(2018, 8, 3))
+                .build());
+        when(albumRepository.findById(id)).thenReturn(album);
+        Optional<Album> result = albumService.getAlbumById(id);
+        assertThat(album).isEqualTo(result);
+    }
+
 
 }
