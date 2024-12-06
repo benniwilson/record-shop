f0e156621257400e569501360bcb9f83c39f6e54
benniwilson <benniwilson7@gmail.com>
2024-12-06 10:24:18 +0000

    feat: Added an exception package to the application which contains an AlbumNotFoundException which is thrown whenever a request contains an id that doesn't exits, whenever this exception is thrown the GlobalExceptionHandler class runs a method which returns a response entity with an error string

---
+++ src/main/java/com/northcoders/record_shop/exception/AlbumNotFoundException.java
@@ -0,0 +1,9 @@
+package com.northcoders.record_shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AlbumNotFoundException extends RuntimeException{
+    public AlbumNotFoundException(String message){super(message);}
+}
