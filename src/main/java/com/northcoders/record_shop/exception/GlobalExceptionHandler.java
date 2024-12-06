7142a3ddb19f7c48ba0f5cb265a93d562beaffbb
benniwilson <benniwilson7@gmail.com>
2024-12-06 15:09:46 +0000

    feat: Added a new NullAttributeException class and a method in the GlobalExceptionHandler class which triggers whenever a NullAttributeException is thrown

--- src/main/java/com/northcoders/record_shop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/northcoders/record_shop/exception/GlobalExceptionHandler.java
@@ -13,4 +13,9 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<Object> handleNullAttributeException(NullAttributeException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
---
+++ src/main/java/com/northcoders/record_shop/exception/NullAttributeException.java
@@ -0,0 +1,5 @@
+package com.northcoders.record_shop.exception;
+
+public class NullAttributeException extends RuntimeException{
+    public NullAttributeException(String message){super(message);}
+}
