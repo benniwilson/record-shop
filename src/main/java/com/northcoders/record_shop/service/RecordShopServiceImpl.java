b13e5fce8eca7d735d1f966055d593630b463220
benniwilson <benniwilson7@gmail.com>
2024-12-10 18:01:16 +0000

    refactor: Changed the implementation of the methods putAlbum and postAlbum such that if any Album data in a request body is invalid or null then a NullAttributeException will be thrown, the request body attributes are checked using the nullAttributeCatcher method

--- src/main/java/com/northcoders/record_shop/service/RecordShopServiceImpl.java
+++ src/main/java/com/northcoders/record_shop/service/RecordShopServiceImpl.java
@@ -1,12 +1,18 @@
 package com.northcoders.record_shop.service;
 
 import com.northcoders.record_shop.exception.AlbumNotFoundException;
+import com.northcoders.record_shop.exception.NullAttributeException;
 import com.northcoders.record_shop.model.Album;
 import com.northcoders.record_shop.repository.RecordShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+
 
 @Service
 public class RecordShopServiceImpl implements RecordShopService {
@@ -24,26 +30,46 @@ public class RecordShopServiceImpl implements RecordShopService {
     @Override
     public Album getAlbumById(Long id) {
         Optional<Album> album = recordShopRepository.findById(id);
-        if (album.isPresent()){
+        if (album.isPresent()) {
             return album.get();
-        }else{
+        } else {
             throw new AlbumNotFoundException(String.format("No Album with id: %s, was found in the system", id));
         }
     }
 
     @Override
-    public Album postAlbum(Album album){
-      return recordShopRepository.save(album);
+    public Album postAlbum(Album album) {
+        Map<String, Boolean> nullCheckMap = nullAttributeCatcher(album);
+        List<String> nullKeys = new ArrayList<>();
+        if (nullCheckMap.containsValue(true)) {
+            for (var e : nullCheckMap.entrySet()) {
+                if (e.getValue().equals(true)) {
+                    nullKeys.add(e.getKey());
+                }
+            }
+            throw new NullAttributeException("The following attributes are invalid: " + nullKeys.toString());
+        }
+        return recordShopRepository.save(album);
     }
 
     @Override
     public Album putAlbum(Album album, long id) {
+        Map<String, Boolean> nullCheckMap = nullAttributeCatcher(album);
+        List<String> nullKeys = new ArrayList<>();
+        if (nullCheckMap.containsValue(true)) {
+            for (var e : nullCheckMap.entrySet()) {
+                if (e.getValue().equals(true)) {
+                    nullKeys.add(e.getKey());
+                }
+            }
+            throw new NullAttributeException("The following attributes are invalid: " + nullKeys.toString());
+        }
         Optional<Album> optionalAlbum = recordShopRepository.findById(id);
-        if (optionalAlbum.isPresent()){
+        if (optionalAlbum.isPresent()) {
             album.setId(id);
             return recordShopRepository.save(album);
-        }else{
-            throw new AlbumNotFoundException(String.format("No Album with id: %s, was found in the system",id));
+        } else {
+            throw new AlbumNotFoundException(String.format("No Album with id: %s, was found in the system", id));
         }
     }
 
@@ -51,51 +77,51 @@ public class RecordShopServiceImpl implements RecordShopService {
     public Album deleteAlbum(long id) {
         Optional<Album> album = recordShopRepository.findById(id);
         Album deletedAlbum;
-        if (album.isPresent()){
+        if (album.isPresent()) {
             deletedAlbum = album.get();
             recordShopRepository.deleteById(id);
             return deletedAlbum;
-        }else{
-            throw new AlbumNotFoundException(String.format("No Album with id: %s, was found in the system",id));
+        } else {
+            throw new AlbumNotFoundException(String.format("No Album with id: %s, was found in the system", id));
         }
     }
 
     @Override
     public Map<String, Boolean> nullAttributeCatcher(Album album) {
         Map<String, Boolean> mapAlbum = new HashMap<>();
-        if (album.getId() == 0){
+        if (album.getId() < 0) {
             mapAlbum.put("id", true);
-        }else{
+        } else {
             mapAlbum.put("id", false);
         }
-        if (album.getName() == null || album.getName().isEmpty()){
+        if (album.getName() == null || album.getName().isEmpty()) {
             mapAlbum.put("name", true);
-        }else{
+        } else {
             mapAlbum.put("name", false);
         }
-        if (album.getArtist() == null || album.getArtist().isEmpty()){
+        if (album.getArtist() == null || album.getArtist().isEmpty()) {
             mapAlbum.put("artist", true);
-        }else{
+        } else {
             mapAlbum.put("artist", false);
         }
-        if (album.getDateReleased() == null){
+        if (album.getDateReleased() == null) {
             mapAlbum.put("dateReleased", true);
-        }else {
+        } else {
             mapAlbum.put("dateReleased", false);
         }
-        if (album.getGenre() == null){
+        if (album.getGenre() == null) {
             mapAlbum.put("genre", true);
-        }else{
+        } else {
             mapAlbum.put("genre", false);
         }
-        if (album.getStock() < 0){
+        if (album.getStock() < 0) {
             mapAlbum.put("stock", true);
-        }else {
+        } else {
             mapAlbum.put("stock", false);
         }
-        if (album.getPrice() <= 0.0){
+        if (album.getPrice() <= 0.0) {
             mapAlbum.put("price", true);
-        }else{
+        } else {
             mapAlbum.put("price", false);
         }
         return mapAlbum;
